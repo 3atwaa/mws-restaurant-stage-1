6caff2001c280b51eb6d92b267a29c031db6025f
Mahmoud Atwa <mahmoudatwa222@hotmail.com>
2018-06-05 07:36:54 +0200

    Service worker updated!

--- sw.js
+++ sw.js
@@ -1,20 +1,23 @@
 //Note::This code is inspired from Udacity's Offline first mobile apps course code.
 
-const cacheName = 'restaurant-reviews-v7';
-const indexURL = 'https://atwamahmoud.github.io/mws-restaurant-stage-1/';
+const cacheName = 'restaurant-reviews-v30';
+let itemsToBeCached = [
+    './index.html',
+    './restaurant.html',
+    './css/styles.css',
+    './js/main.js',
+    './js/dbhelper.js',
+    './js/restaurant_info.js',
+    './data/restaurants.json'
+]
+for(let i = 1; i<=10; i++)
+    itemsToBeCached.push(`./img/${i}.jpg`);
+
 //Caching data on install
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(cacheName).then(cache => {
-           return cache.addAll([//returns a promise
-                'https://atwamahmoud.github.io/mws-restaurant-stage-1/index.html',
-                'https://atwamahmoud.github.io/mws-restaurant-stage-1/restaurant.html',
-                'https://atwamahmoud.github.io/mws-restaurant-stage-1/css/styles.css',
-                'https://atwamahmoud.github.io/mws-restaurant-stage-1/js/main.js',
-                'https://atwamahmoud.github.io/mws-restaurant-stage-1/js/dbhelper.js',
-                'https://atwamahmoud.github.io/mws-restaurant-stage-1/js/restaurant_info.js',
-                'https://atwamahmoud.github.io/mws-restaurant-stage-1/data/restaurants.json'
-            ]);
+           return cache.addAll(itemsToBeCached);//returns a promise
         }).catch(err => {
             console.log(err);
         })
@@ -41,18 +44,30 @@ self.addEventListener('activate', event => {
 self.addEventListener('fetch', event => {
 
     const url = event.request.url;
-    if (url === indexURL || url === indexURL.slice(0, indexURL.length-1) || url === indexURL + 'index.html')
+    const urlObj = new URL(url);
+    const acceptedIndexPathNames = [
+        '/mws-restaurant-stage-1',
+        '/mws-restaurant-stage-1/',
+        '/mws-restaurant-stage-1/index.html',
+        '/',
+        '/index.html'
+    ]
+    const acceptedRestPathNames = [
+        '/mws-restaurant-stage-1/restaurant.html',
+        '/restaurant.html'
+    ]
+    if (acceptedIndexPathNames.includes(urlObj.pathname))
         event.respondWith(
             caches.open(cacheName).then(cache => {
-                return cache.match('https://atwamahmoud.github.io/mws-restaurant-stage-1/index.html').then(resp => resp);
+                return cache.match('./index.html').then(resp => resp);
             }).catch(err => {
                 console.log(err);
             })
         )
-    else if (url.endsWith('restaurant.html?id=', event.request.url.length-1))
+    else if (acceptedRestPathNames.includes(urlObj.pathname))
         event.respondWith(
             caches.open(cacheName).then(cache => {
-                return cache.match('https://atwamahmoud.github.io/mws-restaurant-stage-1/restaurant.html').then(resp => resp);
+                return cache.match('./restaurant.html').then(resp => resp);
             }).catch(err => {
                 console.log(err);
             })
@@ -67,4 +82,5 @@ self.addEventListener('fetch', event => {
                 })
             })
         )
+
 });
